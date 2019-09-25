c59485600c08c43c076987877d0b2d1864739546
ItsFated <384378433@qq.com>
2019-09-25 11:49:14 +0800

    更新抓取日志脚本，改为无限循环抓取，添加时间后缀

--- logcat.bat
+++ logcat.bat
@@ -1,6 +1,8 @@
+
+:loop
 adb wait-for-device
 @echo off
 adb logcat -g
-adb logcat -c
 @echo on
-adb logcat -v threadtime > %1_main.log
+adb logcat -v threadtime > %1_main_%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.log
+goto loop
