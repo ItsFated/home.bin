7eaad92a73dba52666bbab306f0d8859e97134d2
ItsFated <384378433@qq.com>
2019-12-31 11:23:47 +0800

    给logcat脚本的日志文件名添加引号

--- logcat.bat
+++ logcat.bat
@@ -4,5 +4,5 @@ adb wait-for-device
 @echo off
 adb logcat -g
 @echo on
-adb logcat -v threadtime > %1_main_%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.log
+adb logcat -v threadtime > "%1_main_%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.log"
 goto loop
